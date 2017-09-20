bb8545e4c3800ed01f7dd5bcfd181b18d2a5ebc7
Nick Guerrero <nicolasjguerrero@gmail.com>
2017-09-20 15:07:03 -0400

    Started

---
+++ Instakilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instakilo
+//
+//  Created by Nicolas Guerrero on 9/20/17.
+//  Copyright © 2017 Nicolas Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Instakilo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Instakilo
+//
+//  Created by Nicolas Guerrero on 9/20/17.
+//  Copyright © 2017 Nicolas Guerrero. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
