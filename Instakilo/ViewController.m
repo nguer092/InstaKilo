d4af8be30e4ef36f08c6d8b8e2a68856fc427614
Nick Guerrero <nicolasjguerrero@gmail.com>
2017-09-21 12:20:18 -0400

    UI Update

--- Instakilo/ViewController.m
+++ Instakilo/ViewController.m
@@ -38,8 +38,11 @@
     PhotoObject *oldman = [[PhotoObject alloc] initWithName:@"oldman"];
     PhotoObject *levi = [[PhotoObject alloc] initWithName:@"levi"];
     PhotoObject *roy = [[PhotoObject alloc] initWithName:@"roy"];
-    self.photos = @[naruto, luffy, gon, eren, edward];
-    self.masterPhotos = @[kakashi, shanks, oldman, levi, roy];
+    PhotoObject *simon = [[PhotoObject alloc] initWithName:@"simon"];
+    PhotoObject *kamina = [[PhotoObject alloc] initWithName:@"kamina"];
+    self.photos = @[naruto, luffy, gon, eren, edward, simon];
+    self.masterPhotos = @[kakashi, shanks, oldman, levi, roy, kamina];
+
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
@@ -54,10 +57,11 @@
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
-    if(indexPath.section == 0)
-    cell.photo = self.photos[indexPath.row];
-    else if (indexPath.section == 1)
-        cell.photo = self.masterPhotos[indexPath.row];
+    if (indexPath.section == 0){
+        cell.photo = self.photos[indexPath.row];}
+    else if (indexPath.section == 1){
+        cell.photo = self.masterPhotos[indexPath.row];}
+    
     return cell;
 }
 
